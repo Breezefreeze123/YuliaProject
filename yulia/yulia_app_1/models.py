8ac85c2638568596b6c6b1e0e3f85c01d317eab9
loskutova.yulia <loskutova.yulia@gmail.com>
2024-12-11 18:44:46 +0300

    DB - db_Category created, Foreignkey (Many-to-one)

--- yulia/yulia_app_1/models.py
+++ yulia/yulia_app_1/models.py
@@ -14,9 +14,17 @@ class Coffee(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status, default=Status.PUBLISHED)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('show_menu', kwargs={'menu_slug': self.slug})
+    
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
