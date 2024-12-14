3e17c8580b59b7b55e92b028a895e21aebc145e7
loskutova.yulia <loskutova.yulia@gmail.com>
2024-12-14 13:32:04 +0300

    DB - TagTable added

--- yulia/yulia_app_1/models.py
+++ yulia/yulia_app_1/models.py
@@ -14,7 +14,8 @@ class Coffee(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status, default=Status.PUBLISHED)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='category')
+    tag = models.ManyToManyField('TagTable', blank=True, related_name='tagtable')
 
     def __str__(self):
         return self.title
@@ -32,3 +33,9 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('show_category', kwargs={'cat_slug': self.slug})
     
+class TagTable(models.Model):
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.tag
