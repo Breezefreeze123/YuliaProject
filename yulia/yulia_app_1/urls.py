a53540dd24d07ad2b0a10225a12e0845c63bf6f2
loskutova.yulia <loskutova.yulia@gmail.com>
2025-06-03 20:56:46 +0300

    CBV Part 2 (ListView)

--- yulia/yulia_app_1/urls.py
+++ yulia/yulia_app_1/urls.py
@@ -2,16 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    #path('menu/', views.menu, name = 'menu'),
+    path('', views.Home.as_view(), name = 'home'),
     path('menu/', views.Menu.as_view(), name = 'menu'),
-    #path('news/', views.news, name = 'news'),
     path('news/', views.News.as_view(), name = 'news'),
-    path('contacts/', views.contacts, name = 'contacts'),
-    path('add_product/', views.add_product, name = 'add_product'),
-    #path('signin/', views.signin, name = 'signin'), 
+    path('contacts/', views.Contacts.as_view(), name = 'contacts'),
+    path('add_product/', views.add_product, name = 'add_product'), 
     path('signin/', views.SignIn.as_view(), name = 'signin'),
     path('menu/<slug:menu_slug>/', views.show_menu, name = 'show_menu'),
-    path('menu/category/<slug:cat_slug>/', views.show_category, name = 'show_category'),
-    path('menu/tag/<slug:tag_slug>/', views.show_tag, name = 'show_tag'),
+    path('menu/category/<slug:cat_slug>/', views.ShowCategory.as_view(), name = 'show_category'),
+    path('menu/tag/<slug:tag_slug>/', views.ShowTag.as_view(), name = 'show_tag'),
 ]
--- yulia/yulia_app_1/views.py
+++ yulia/yulia_app_1/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.db.models import Count, Sum, Avg, Max, Min
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import Coffee, Category, TagTable, Gost, UploadFiles
 from .forms import AddProductForm, UploadFileForm
@@ -19,31 +19,40 @@ main_menu = [
     {'title': 'Sign in', 'url_name': 'signin'},
 ]
 
-def home(request):
-    data = {
+# Заменена на CBV Home(TemplateView)
+# def home(request):
+#     data = {
+#         'title': 'Homepage of Yulia Coffeeshop',
+#         'main_menu': main_menu,
+#     }
+#     return render(request, 'home.html', context=data)
+
+class Home(TemplateView):
+    template_name = 'home.html'
+    extra_context = {
         'title': 'Homepage of Yulia Coffeeshop',
         'main_menu': main_menu,
     }
-    return render(request, 'home.html', context=data)
 
-""" Заменена на CBW Menu >>>
-def menu(request):
+# Заменена на CBV Menu(ListView)
+# def menu(request):
 
-    all_categories = Category.objects.annotate(total=Count('category')).filter(total__gt=0) #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля
-    all_tags = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0) #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
+#     all_categories = Category.objects.annotate(total=Count('category')).filter(total__gt=0) #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля
+#     all_tags = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0) #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
   
-    data = {
-        'title': 'Coffee Menu',
-        'title_tags': 'Тэги: ',
-        'all_categories': all_categories,
-        'main_menu': main_menu,
-        'all_tags': all_tags,
-    }
-    return render(request, 'menu/menu.html', context=data)
-"""
+#     data = {
+#         'title': 'Coffee Menu',
+#         'title_tags': 'Тэги: ',
+#         'all_categories': all_categories,
+#         'main_menu': main_menu,
+#         'all_tags': all_tags,
+#     }
+#     return render(request, 'menu/menu.html', context=data)
     
-class Menu(TemplateView):
+class Menu(ListView):
+    #model = Coffee
     template_name = 'menu/menu.html'
+    context_object_name = 'all_products'
     extra_context = {
         'title': 'Coffee Menu',
         'title_tags': 'Тэги: ',
@@ -53,45 +62,86 @@ class Menu(TemplateView):
         #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
         'all_tags': TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0),
     }
+    
+    def get_queryset(self):
+        return Coffee.objects.all().filter(is_published=1)
 
-def show_category(request, cat_slug):
+# Заменена на CBV ShowCategory(ListView)
+# def show_category(request, cat_slug):
 
-    all_categories = Category.objects.annotate(total=Count('category')).filter(total__gt=0) #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля
-    all_tags = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0) #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
+#     all_categories = Category.objects.annotate(total=Count('category')).filter(total__gt=0) #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля
+#     all_tags = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0) #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
 
-    category = get_object_or_404(Category, slug=cat_slug)
-    post = Coffee.objects.filter(cat_id=category.pk)
+#     category = get_object_or_404(Category, slug=cat_slug)
+#     post = Coffee.objects.filter(cat_id=category.pk)
 
-    data = {
-        'title': f'Раздел: {category.title}',
-        'title_tags': 'Тэги: ',
-        'all_categories': all_categories,
-        'main_menu': main_menu,
-        'category': category,
-        'post': post,
-        'all_tags': all_tags,
+#     data = {
+#         'title': f'Раздел: {category.title}',
+#         'title_tags': 'Тэги: ',
+#         'all_categories': all_categories,
+#         'main_menu': main_menu,
+#         'category': category,
+#         'post': post,
+#         'all_tags': all_tags,
         
-    }
-    return render(request, 'menu/menu.html', context=data)
+#     }
+#     return render(request, 'menu/menu.html', context=data)
 
-def show_tag(request, tag_slug):
+class ShowCategory(ListView):
+    template_name = 'menu/menu.html'
+    context_object_name = 'post'
+    allow_empty = False
 
-    all_categories = Category.objects.annotate(total=Count('category')).filter(total__gt=0) #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля
-    all_tags = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0) #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
+    def get_queryset(self):
+        return Coffee.objects.filter(cat__slug=self.kwargs['cat_slug'])
 
-    tag = get_object_or_404(TagTable, slug=tag_slug)
-    tag_selection = tag.tagtable.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['post'][0].cat
+        context['title'] = 'Категория: ' + cat.title
+        context['title_tags'] = 'Тэги: '
+        context['all_categories'] = Category.objects.annotate(total=Count('category')).filter(total__gt=0)
+        context['main_menu'] = main_menu
+        context['all_tags'] = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0)
+        return context
 
-    data = {
-        'title': f'Тэг: {tag.tag}',
-        'title_tags': 'Тэги: ',
-        'main_menu': main_menu,
-        'all_categories': all_categories,
-        'all_tags': all_tags,
-        'tag': tag,
-        'tag_selection': tag_selection,
-    }
-    return render(request, 'menu/menu.html', context=data)
+# Заменена на CBV ShowTag(ListView)
+# def show_tag(request, tag_slug):
+
+#     all_categories = Category.objects.annotate(total=Count('category')).filter(total__gt=0) #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля
+#     all_tags = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0) #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуля
+
+#     tag = get_object_or_404(TagTable, slug=tag_slug)
+#     tag_selection = tag.tagtable.all()
+
+#     data = {
+#         'title': f'Тэг: {tag.tag}',
+#         'title_tags': 'Тэги: ',
+#         'main_menu': main_menu,
+#         'all_categories': all_categories,
+#         'all_tags': all_tags,
+#         'tag': tag,
+#         'tag_selection': tag_selection,
+#     }
+#     return render(request, 'menu/menu.html', context=data)
+
+class ShowTag(ListView):
+    template_name = 'menu/menu.html'
+    context_object_name = 'tag_selection'
+    allow_empty = False
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(TagTable, slug=self.kwargs['tag_slug'])
+        return self.tag.tagtable.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Тэг: ' + self.tag.tag
+        context['title_tags'] = 'Тэги: '
+        context['all_categories'] = Category.objects.annotate(total=Count('category')).filter(total__gt=0)
+        context['main_menu'] = main_menu
+        context['all_tags'] = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0)
+        return context
 
 def show_menu(request, menu_slug):
 
@@ -109,6 +159,25 @@ def show_menu(request, menu_slug):
     }
     return render(request, 'menu/menu_sections.html', context=data)
 
+# class ShowMenu(TemplateView):
+#     template_name = 'menu/menu_sections.html'
+#     extra_context = {
+#         'title_tags': 'Тэги: ',
+#         'post': get_object_or_404(Coffee, slug=menu_slug),
+#         'main_menu': main_menu,
+#         'all_categories': Category.objects.annotate(total=Count('category')).filter(total__gt=0), #Проверка, что количество категорий в Category, связанных со статьями в Coffee, больше нуля,
+#         'all_tags': TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0), #Проверка, что количество тэгов в TagTable, связанных со статьями в Coffee, больше нуляall_tags,
+#     }
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title_tags'] = 'Тэги: '
+#         context['main_menu'] = main_menu
+#         context['all_categories'] = Category.objects.annotate(total=Count('category')).filter(total__gt=0)
+#         context['all_tags'] = TagTable.objects.annotate(total=Count('tagtable')).filter(total__gt=0)
+#         context['post'] = get_object_or_404(Coffee, slug=menu_slug)
+#         return context
+
 def handle_uploaded_file(f):
     #new_rename = str(uuid.uuid4()) + f.name
     
@@ -122,35 +191,34 @@ def handle_uploaded_file(f):
         for chunk in f.chunks():
             destination.write(chunk)
 
-""" Заменена на CBW News >>>
-def news(request):
+# Заменена на CBV News(View)
+# def news(request):
 
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+#     if request.method == 'POST':
+#         form = UploadFileForm(request.POST, request.FILES)
         
-        if form.is_valid():
-            try:
-                # Стандартная загрузка файлов
-                # handle_uploaded_file(form.cleaned_data['file'])
+#         if form.is_valid():
+#             try:
+#                 # Стандартная загрузка файлов
+#                 # handle_uploaded_file(form.cleaned_data['file'])
                 
-                # Загрузка файлов через модель
-                instance = UploadFiles(upload=form.cleaned_data['file'])
-                instance.save()
-            except Exception as e:
-                form.add_error(None, "Ошибка загрузки файла")
-                form.add_error(None, e)
+#                 # Загрузка файлов через модель
+#                 instance = UploadFiles(upload=form.cleaned_data['file'])
+#                 instance.save()
+#             except Exception as e:
+#                 form.add_error(None, "Ошибка загрузки файла")
+#                 form.add_error(None, e)
 
-    else:
-        form = UploadFileForm()    
+#     else:
+#         form = UploadFileForm()    
         
 
-    data = {
-        'title': 'Coffee News',
-        'main_menu': main_menu,
-        'form': form,
-    }
-    return render(request, 'news/news.html', context=data)
-"""
+#     data = {
+#         'title': 'Coffee News',
+#         'main_menu': main_menu,
+#         'form': form,
+#     }
+#     return render(request, 'news/news.html', context=data)
     
 class News(View):
     def get(self, request):
@@ -179,12 +247,27 @@ class News(View):
         }
         return render(request, 'news/news.html', context=data)
 
-def contacts(request):
-    data = {
+# Заменена на CBV Contacts(TemplateView)
+# def contacts(request):
+#     data = {
+#         'title': 'Coffeeshop contacts',
+#         'main_menu': main_menu,
+#     }
+#     return render(request, 'contacts/contacts.html', context=data)
+
+# class Contacts(TemplateView):
+#     template_name = 'contacts/contacts.html'
+#     extra_context = {
+#         'title': 'Coffeeshop contacts',
+#         'main_menu': main_menu,
+#     }
+
+class Contacts(TemplateView):
+    template_name = 'contacts/contacts.html'
+    extra_context = {
         'title': 'Coffeeshop contacts',
         'main_menu': main_menu,
     }
-    return render(request, 'contacts/contacts.html', context=data)
 
 def add_product(request):
     
@@ -236,26 +319,26 @@ def add_product(request):
     }
     return render(request, 'add_product/add_product.html', context=data)
 
-""" Заменена на CBW SignIn >>>
-def signin(request):
-    data = {
-        'title': 'Sign in',
-        'main_menu': main_menu,
-    }
-    return render(request, 'signin/signin.html', context=data)
-"""
-""" Заменена на CBW SignIn через TemplateView >>>
-class SignIn(View):
-    def get(self, request):
-        data = {
-            'title': 'Sign in',
-            'main_menu': main_menu,
-        }
-        return render(request, 'signin/signin.html', context=data)
+# Заменена на CBV SignIn(View)
+# def signin(request):
+#     data = {
+#         'title': 'Sign in',
+#         'main_menu': main_menu,
+#     }
+#     return render(request, 'signin/signin.html', context=data)
 
-    def post(self, request):
-        pass
-"""
+# Заменена на CBV SignIn(TemplateView)
+# class SignIn(View):
+#     def get(self, request):
+#         data = {
+#             'title': 'Sign in',
+#             'main_menu': main_menu,
+#         }
+#         return render(request, 'signin/signin.html', context=data)
+
+#     def post(self, request):
+#         pass
+
 class SignIn(TemplateView):
     template_name = 'signin/signin.html'
     extra_context = {
